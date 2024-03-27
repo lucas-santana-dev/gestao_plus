7e9cbc57d32494b7826d27818443b8c44c227059
Lucas Santana <admin@Mac-mini-de-Lucas.local>
2024-03-27 18:36:59 -0300

    Reformulação do footer do sistema: Agora o footer conta com o botão para dar acesso ao menu principal com as opções do sistema

--- src/view/components/Footer.java
+++ src/view/components/Footer.java
@@ -7,11 +7,26 @@ import java.awt.*;
 
 public class Footer extends JPanel {
     private JLabel label;
-
+    private JFrame menuPrincipal = new MenuPrincipal();
     public Footer(String text) {
-        setBackground(Color.LIGHT_GRAY);
-        // Cria um JLabel com o texto recebido como parâmetro
+        JPanel menuPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+        setLayout(new BorderLayout());
+
+        JButton menuButton = new JButton("\u2630");
+        menuButton.setFont(new Font("Arial", Font.PLAIN, 20));
+        menuButton.setBorderPainted(false);
+        menuButton.addActionListener((e)->{
+            menuPrincipal.setVisible(true);
+        });
+        menuPanel.add(menuButton);
+        JLabel menuLabel = new JLabel("Menu");
+        menuLabel.setFont(new Font("Arial", Font.BOLD, 14));
+        menuPanel.add(menuLabel);
+        add(menuPanel, BorderLayout.WEST);
+
+
         label = new JLabel(text);
-        add(label, BorderLayout.SOUTH); // Adiciona o JLabel ao JPanel
+        label.setHorizontalAlignment(SwingConstants.CENTER);
+        add(label, BorderLayout.CENTER);
     }
 }
---
+++ src/view/components/MenuPrincipal.java
@@ -0,0 +1,2 @@
+package view.components;public class MenuPrincipal {
+}
