9a08e21974d619baf30a602df2761eb7be55d054
Lucas Santana <lucassantanaads@gmail.com>
2024-05-06 13:19:42 -0300

    feat: Conclusão do front da tela de login

--- src/main/view/frames/login/FormLoginPanel.java
+++ src/main/view/frames/login/FormLoginPanel.java
@@ -2,7 +2,6 @@ package main.view.frames.login;
 
 import javax.swing.*;
 import java.awt.*;
-
 public class FormLoginPanel extends JPanel {
 
     public FormLoginPanel() {
@@ -10,37 +9,44 @@ public class FormLoginPanel extends JPanel {
     }
 
     private void initializeUI() {
+        // Define o layout GridBagLayout para maior flexibilidade
         setLayout(new GridBagLayout());
-        setBackground(Color.LIGHT_GRAY);
 
-        // Configuração do GridBagConstraints para posicionar os componentes
+        // Adiciona espaçamento ao redor do formulário
+        setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20));
+
         GridBagConstraints gbc = new GridBagConstraints();
         gbc.gridx = 0;
         gbc.gridy = 0;
-        gbc.insets = new Insets(10, 10, 10, 10);
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+        gbc.insets = new Insets(5, 5, 5, 5); // Espaçamento interno
 
         // Label e campo de entrada para o usuário
-        JLabel userLabel = new JLabel("Usuário:");
+        JLabel userLabel = new JLabel("Usuario:");
         add(userLabel, gbc);
 
-        gbc.gridx++;
-        JTextField userField = new JTextField(15);
+        gbc.gridy++;
+        JTextField userField = new JTextField(20);
+        userField.setPreferredSize(new Dimension(280, 30)); // Ajusta a largura e altura do campo de texto
         add(userField, gbc);
 
-        gbc.gridx = 0;
-        gbc.gridy++;
         // Label e campo de entrada para a senha
+        gbc.gridy++;
         JLabel passwordLabel = new JLabel("Senha:");
         add(passwordLabel, gbc);
 
-        gbc.gridx++;
-        JPasswordField passwordField = new JPasswordField(15);
+        gbc.gridy++;
+        JPasswordField passwordField = new JPasswordField(20);
+        passwordField.setPreferredSize(new Dimension(280, 30)); // Ajusta a largura e altura do campo de texto
         add(passwordField, gbc);
 
-        gbc.gridx = 0;
-        gbc.gridy++;
         // Botão de login
+        gbc.gridy++;
         JButton loginButton = new JButton("Entrar");
+        loginButton.setPreferredSize(new Dimension(280, 35)); // Ajusta a largura e altura do botão
         add(loginButton, gbc);
     }
+
+
+
 }
